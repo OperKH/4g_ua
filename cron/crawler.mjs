326c7be3b9362677b023cb968406507b0e01fe2c
OperKH <oper.kh@gmail.com>
2018-05-14 20:34:16 +0300

    Update equipment.

--- cron/crawler.mjs
+++ cron/crawler.mjs
@@ -29,7 +29,7 @@ const getEquipmentBrandByModelName = modelName => {
     )
   ) {
     return 'Ericsson'
-  } else if (/Nokia|Flexi Multiradio/i.test(modelName)) {
+  } else if (/Nokia|Flexi Multiradio|BTS Optima|BTS Supreme/i.test(modelName)) {
     return 'Nokia'
   } else if (/BTS 3803|BTS3812|BTS 3900|DBS 3800|DTS 3803C|DBS\s?3900/i.test(modelName)) {
     return 'Huawei'
