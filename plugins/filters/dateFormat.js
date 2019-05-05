d9e58bb7bf787c9e25f86df6e30ed932e84b9b80
OperKH <oper.kh@gmail.com>
2019-05-05 14:39:38 +0300

    Fix SSR date format filter.

--- plugins/filters/dateFormat.js
+++ plugins/filters/dateFormat.js
@@ -3,5 +3,13 @@ import Vue from 'vue'
 Vue.filter('formatDate', value => {
   const date = new Date(value)
   if (!isFinite(date)) return value
-  return date.toLocaleString('ua-UA').replace(',', '')
+  const year = date.getFullYear()
+  const mon = date.getMonth() + 1
+  const day = date.getDate()
+  const hours = date.getHours()
+  const minutes = date.getMinutes()
+  const seconds = date.getSeconds()
+  return `${day < 10 ? '0' + day : day}.${mon < 10 ? '0' + mon : mon}.${year} ${hours < 10 ? '0' + hours : hours}:${
+    minutes < 10 ? '0' + minutes : minutes
+  }:${seconds < 10 ? '0' + seconds : seconds}`
 })
